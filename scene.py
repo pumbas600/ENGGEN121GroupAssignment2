d2b6fcd4b16d29034bdbc1d223cdf6f513e84c39
pumbas600 <joshjeffers600@gmail.com>
2021-05-25 17:38:05 +1200

    Reference angle to cart FBD

--- scene.py
+++ scene.py
@@ -31,6 +31,44 @@ class Task2Dot4(Scene):
         self.wait(0.2)
         self.play(Write(equation[2]), run_time=2)
 
+class MovingAngle(Scene):
+    def construct(self):
+        rotation_center = LEFT
+
+        theta_tracker = ValueTracker(110)
+        line1 = Line(LEFT, RIGHT)
+        line_moving = Line(LEFT, RIGHT)
+        line_ref = line_moving.copy()
+        line_moving.rotate(
+            theta_tracker.get_value() * DEGREES, about_point=rotation_center
+        )
+        a = Angle(line1, line_moving, radius=0.5, other_angle=False)
+        te = MathTex(r"\theta").next_to(a, RIGHT)
+
+        self.add(line1, line_moving, a, te)
+        self.wait()
+
+        line_moving.add_updater(
+            lambda x: x.become(line_ref.copy()).rotate(
+                theta_tracker.get_value() * DEGREES, about_point=rotation_center
+            )
+        )
+
+        a.add_updater(
+            lambda x: x.become(Angle(line1, line_moving, radius=0.5, other_angle=False))
+        )
+        te.add_updater(
+            lambda x: x.move_to(
+                Angle(
+                    line1, line_moving, radius=0.5 + 3 * SMALL_BUFF, other_angle=False
+                ).point_from_proportion(0.5)
+            )
+        )
+
+        self.play(theta_tracker.animate.set_value(40))
+        self.play(theta_tracker.animate.increment_value(140))
+        self.play(te.animate.set_color(RED), run_time=0.5)
+        self.play(theta_tracker.animate.set_value(350))
 
 class FBDs(Scene):
 
@@ -48,6 +86,10 @@ class FBDs(Scene):
         yLabel = Tex("y").scale(0.8).next_to(y, UP)
 
         return Group(x, y, xLabel, yLabel)
+
+    def displayNumberPlane(self):
+        numberPlane = NumberPlane()
+        self.add(numberPlane)
 
     def suspendedMassFBD(self):
         square = Square(radius=1)
@@ -79,21 +121,34 @@ class FBDs(Scene):
         tensionForce = Arrow(start=ORIGIN, end=RIGHT * 2, color=ORANGE).rotate(self.inclineAngle, about_point=ORIGIN)
 
         normalLabel = Tex("N", color=ORANGE).next_to(normalForce, UP)
-        weightLabel = MathTex("m_{s}g", color=ORANGE).next_to(weightForce, DOWN)
+        weightLabel = MathTex("m_{c}g", color=ORANGE).next_to(weightForce, DOWN)
         tensionLabel = Tex("T", color=ORANGE).next_to(tensionForce, RIGHT)
+
+        surfaceLine = Line(start=LEFT * 2, end=RIGHT * 2).rotate(self.inclineAngle).shift(DOWN * 0.78)
+        referenceLine = Line(start=surfaceLine.get_start(), end=surfaceLine.get_start() + RIGHT * 1.7)
+        angle = Angle(referenceLine, surfaceLine, radius=1.3, other_angle=False)
+        theta =  MathTex(r"\theta").next_to(angle, RIGHT).scale(0.7)
+
+        angleGroup = Group(surfaceLine, referenceLine, angle, theta)
 
         cs = self.generateCS().next_to(cart, LEFT).rotate(self.inclineAngle)
 
         title = Tex("FBD of Cart").next_to(normalLabel, UP)
 
-        group = Group(title, cart, normalForce, weightForce, tensionForce, normalLabel, weightLabel, tensionLabel, cs)
-
-        self.play(Write(title), GrowFromCenter(cart))
-        self.play(FadeIn(normalForce), FadeIn(weightForce), FadeIn(tensionForce), FadeIn(cs))
-        self.play(Write(normalLabel), Write(weightLabel), Write(tensionLabel))
+        fbdGroup = Group(title, cart, normalForce, weightForce, tensionForce, normalLabel, weightLabel, tensionLabel, cs,
+                      angleGroup)
+        self.play(
+            Write(title), Write(theta), FadeIn(angle), Create(surfaceLine), Create(referenceLine), GrowFromCenter(cart)
+        )
+        self.play(
+            Create(normalForce), Create(weightForce), Create(tensionForce), FadeIn(cs)
+        )
+        self.play(
+            Write(normalLabel), Write(weightLabel), Write(tensionLabel)
+        )
         self.wait(2)
-        self.play(group.animate.scale(0.4))
-        self.play(group.animate.shift(LEFT * 5 + UP * 1.5))
+        self.play(fbdGroup.animate.scale(0.4))
+        self.play(fbdGroup.animate.shift(LEFT * 5 + UP * 1.5))
 
     def construct(self):
         self.cartFBD()
